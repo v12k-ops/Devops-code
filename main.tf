1f58c88e1a9110aed5c26123f08471d05465b6ce
v12k-ops <vikasvkulkarni@gmail.com>
2025-07-05 12:54:09 +0530

    new rg added

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "tcsrg" {
   name     = "tcs-rg"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "tcsrg1" {
+  name     = "tcs-rg1"
+  location = "West Europe"
+}
